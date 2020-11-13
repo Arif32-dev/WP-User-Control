f1a4d31017c8592b4201f85fde932718d4a11009
Arif32-dev <dragonwarrior062@gmail.com>
2020-11-13 15:21:38 +0600

    plugin blocked page func modified

--- Includes/ajax_request.php
+++ Includes/ajax_request.php
@@ -10,6 +10,9 @@ class Ajax_Request {
         /* Form submission ajax request */
         add_action('wp_ajax_wpuc_form_submit', [$this, 'wpuc_form_submit']);
         add_action('wp_ajax_nopriv_wpuc_form_submit', [$this, 'wpuc_form_submit']);
+        /* Form login ajax request */
+        add_action('wp_ajax_wpuc_form_login', [$this, 'wpuc_form_login']);
+        add_action('wp_ajax_nopriv_wpuc_form_login', [$this, 'wpuc_form_login']);
     }
     public function wpuc_form_submit() {
         if ($_POST['action'] != 'wpuc_form_submit') {
@@ -67,6 +70,61 @@ class Ajax_Request {
             }
         }
 
+        wp_die();
+    }
+    public function wpuc_form_login() {
+        if ($_POST['action'] != 'wpuc_form_login') {
+            wp_die();
+        }
+
+        parse_str($_POST['form_data'], $parsed_data);
+
+        if (!isset($parsed_data['wpuc_login_form_nonce']) || !wp_verify_nonce($parsed_data['wpuc_login_form_nonce'],  'wpuc_form_login_action')) {
+            wp_die();
+        }
+
+        if (
+            (!isset($parsed_data['user_login']) && $parsed_data['user_login'] == "") ||
+            (!isset($parsed_data['user_password']) && $parsed_data['user_password'] == "")
+        ) {
+            wp_die('empty_field');
+        }
+
+        $sanitzed_parsed_data = [
+            'user_login' => sanitize_text_field($parsed_data['user_login']),
+            'user_password' => sanitize_text_field($parsed_data['user_password']),
+        ];
+        self::user_login($sanitzed_parsed_data);
+    }
+    public static function user_login(array $sanitzed_parsed_data) {
+        $cred = [
+            'user_login' => $sanitzed_parsed_data['user_login'],
+            'user_password' => $sanitzed_parsed_data['user_password'],
+            'remember' => true,
+        ];
+        $res = wp_signon($cred, true);
+        if (!is_wp_error($res)) {
+            $user_id = $res->data->ID;
+            wp_set_current_user($user_id);
+            wp_set_auth_cookie($user_id, true);
+            echo 'success';
+        } else {
+            if (array_key_exists("incorrect_password", $res->errors)) {
+                if ($res->errors['incorrect_password'][0]) {
+                    echo 'Incorrect Password';
+                }
+            }
+            if (array_key_exists("invalid_username", $res->errors)) {
+                if ($res->errors['invalid_username'][0]) {
+                    echo 'Invalid username';
+                }
+            }
+            if (array_key_exists("invalid_email", $res->errors)) {
+                if ($res->errors['invalid_email'][0]) {
+                    echo 'Invalid email';
+                }
+            }
+        }
         wp_die();
     }
 }
--- Includes/protect_pages.php
+++ Includes/protect_pages.php
@@ -25,10 +25,73 @@ class Protect_Pages {
                     if (is_user_logged_in()) {
                         wp_safe_redirect($custom_url);
                     } else {
-                        wp_safe_redirect($login_url);
+                        echo self::login_form($home_url);
+                        self::form_script();
+                        wp_die();
                     }
                 }
             }
         }
+    }
+    public static function login_form($url) {
+        $form = '<div id="container pp_container">
+            <div id="wpuc_alert_box">
+            </div>
+            <form action="" method="POST" id="pp_form">
+                ' . wp_nonce_field('wpuc_form_login_action', 'wpuc_login_form_nonce', true, false) . '
+                <div class="form-group vertical">
+                    <label for="user_login">Email:</label>
+                    <input style="padding: 3px 20px;" type="text" class="form-control" required name="user_login" id="user_login" placeholder="Enter your email or username">
+                </div>
+                <div class="form-group vertical">
+                    <label for="user_password">Password</label>
+                    <input style="padding: 3px 20px;" type="password" name="user_password" required class="form-control" id="user_password" placeholder="Enter your password">
+                </div>
+                <button type="submit" class="btn btn-secondary">Sign In</button>
+            </form>
+            <h5><a href="' . $url . '">Back to Home</a></h5>
+        </div>';
+        return $form;
+    }
+    public static function form_script() { ?>
+
+        <script>
+            jQuery(document).ready(function($) {
+                let alert_box = $('#wpuc_alert_box');
+                let form = $('#pp_form');
+                form.on('submit', function(e) {
+                    e.preventDefault();
+                    let formData = $(e.currentTarget).serialize();
+                    $.ajax({
+                        url: '<?php echo admin_url('admin-ajax.php') ?>',
+                        data: {
+                            action: 'wpuc_form_login',
+                            form_data: formData
+                        },
+                        type: 'post',
+                        success: res => {
+                            console.log(res);
+                            if (res == 'success') {
+                                alert_box.removeClass('wpuc_fail').addClass('wpuc_success').html(`
+                                    Logged In Successfully,
+                                    `).hide().slideDown(400, function() {
+                                    window.location.href = '<?php echo home_url() ?>';
+                                });
+                            } else {
+                                alert_box.removeClass('wpuc_success').addClass('wpuc_fail').html(`
+                                    ${res.replace(/"/g, "")}
+                            `).hide().slideDown();
+                            }
+                        },
+                        error: err => {
+                            alert_box.removeClass('wpuc_success').addClass('wpuc_fail').html(`
+                                    Something Went Wrong.
+                            `).hide().slideDown();
+                        }
+                    })
+                })
+            })
+        </script>
+<?php
     }
 }
